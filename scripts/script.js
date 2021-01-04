015e4cbd0378cd7a33c28c8aedda01faee19d4e1
Lena Blechmann <lenablechmann@gmail.com>
2021-01-04 19:00:04 +0100

    create a non dynamic grid + style css grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
 // create a variable resolution, which will save user input from the #user-resolution
 document.addEventListener('DOMContentLoaded', function() {
     let resolution = 16;
-    // selecting the parent element for all the divs
-    const canvas = document.querySelector('#sketch-pad');
-    let pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    canvas.appendChild(pixel);
+    createGrid(resolution);
 });
+
+function createGrid(resolution){
+    const canvas = document.querySelector('#sketch-pad');
+        for (let column = 0; column < resolution * resolution; column++){
+            let pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            canvas.appendChild(pixel);
+        }
+}
 // it should be 16 default, but user will be able to change it
 
 // create 16x16 divs inside of the #sketch-pad div with javascript using resolution as limit
