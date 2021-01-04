d6f3bf8c9a1b801fdf4f8954be011cf6bb399a1a
Lena Blechmann <lenablechmann@gmail.com>
2021-01-04 15:29:54 +0100

    create baseline pseudo code

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// create a variable resolution, which will save user input from the #user-resolution
+// it should be 16 default, but user will be able to change it
+
+// create 16x16 divs inside of the #sketch-pad div with javascript using resolution as limit
+// function createCanvasDivs
+
+// the same function should be called if user changes resolution with the form
+
+// change class of the created divs on hover
+
+// create various classes for the palette and change the class depending on user choice
