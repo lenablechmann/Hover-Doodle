c1b0bfee5b0343cec7921e3c6f96d27b14ca6732
Lena Blechmann <lenablechmann@gmail.com>
2021-01-04 17:01:30 +0100

    add first div into containter w js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 // create a variable resolution, which will save user input from the #user-resolution
+document.addEventListener('DOMContentLoaded', function() {
+    let resolution = 16;
+    // selecting the parent element for all the divs
+    const canvas = document.querySelector('#sketch-pad');
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    canvas.appendChild(pixel);
+});
 // it should be 16 default, but user will be able to change it
 
 // create 16x16 divs inside of the #sketch-pad div with javascript using resolution as limit
