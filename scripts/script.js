9c668f0c0dc7bb5c3cd8adc61096cf9397f9d3d1
Lena Blechmann <lenablechmann@gmail.com>
2021-01-05 15:31:09 +0100

    Add eventlisteners to canvas and its each pixel

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,12 @@
 document.addEventListener('DOMContentLoaded', function() {
         if(!localStorage.getItem('resolution')) {
         // using localStorage to save user choice, otherwise site resets on 
-        // submit. tried return false on form submit, but the grid wont fill fully
+        // submit. tried "return false" on form submit, but the grid wont fill fully
         localStorage.setItem('resolution', 16);
     }
     let resolution = localStorage.getItem('resolution');
     createCanvasGrid(resolution);
+    // accomodate for the updated resolution on the resolution slider starter value
 
     document.querySelector("form").onsubmit = function (){
         // changes canvas size upon user slider change
@@ -14,6 +15,9 @@ document.addEventListener('DOMContentLoaded', function() {
         createCanvasGrid(resolution);
         localStorage.setItem('resolution', resolution);
     };
+
+    let sketchPad = document.querySelector('#sketch-pad');
+    sketchPad.addEventListener('mouseenter', recolorPixel);
 });
 
 function createCanvasGrid(resolution){
@@ -21,13 +25,23 @@ function createCanvasGrid(resolution){
     // dynamically changing resolution of the css grid
     canvas.style.gridTemplate = `repeat(${resolution}, 1fr) / repeat(${resolution}, 1fr)`
     // creating the grid
-    for (let column = 0; column < resolution * resolution; column++){
+    for (let cell = 0; cell < resolution * resolution; cell++){
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
         canvas.appendChild(pixel);
     }
 }
 
+function recolorPixel(){
+    console.log("wow, you hovered");
+    
+    const pixelsArray = document.querySelectorAll('.pixel');
+    pixelsArray.forEach((element) => {
+        element.addEventListener('mouseenter', () => {
+            console.log("ooh a pixel");
+        });
+    });
+}
 
 // the same function should be called if user changes resolution with the form
 
